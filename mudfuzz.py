ab81436498a792abe2e37d1c418a968d73cedf38
Virthe <virthe@accursed-lands.net>
2021-04-08 12:33:35 +0100

    interactive telnet

--- mudfuzz.py
+++ mudfuzz.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+from telnetlib import Telnet
+
 def main ():
 	print ( "MUD Fuzz" )
+
+	with Telnet ( 'localhost', 8000 ) as tn:
+		tn.interact ()
 
 if __name__ == "__main__":
 	main ();
