3eb32fecb141e0165c63256c2bbc651de542eb8e
Virthe <virthe@accursed-lands.net>
2021-04-08 16:00:43 +0100

    mud login

--- mudfuzz.py
+++ mudfuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import json, threading, queue, time
+import json, threading, queue, time, re
 from telnetlib import Telnet
 from enum import Enum, auto
 
@@ -31,10 +31,40 @@ class MudFuzz:
         if ( self.connection is None ):
             return
 
-        rcv = self.connection.rcv_queue.get ()
+        try:
+            rcv = self.connection.rcv_queue.get ( block=False )
+        except queue.Empty:
+            pass
+        else:
+            self._process_incoming_data ( rcv )
 
-        if ( len ( rcv ) > 0 ):
-            print ( rcv )
+    def _process_incoming_data ( self, rcv ):
+
+        text = rcv.decode ( 'utf-8' )
+
+        if ( len ( text ) > 0 ):
+            print ( text )
+
+        if ( self.state is MudFuzzState.AWAIT_USER ):
+           if ( re.search ( self.config_data [ "user_prompt" ], text ) ):
+               print ( "User prompt detected." )
+               self._send_string ( self.config_data [ "user" ] )
+               self.state = MudFuzzState.AWAIT_PASS
+
+        if ( self.state is MudFuzzState.AWAIT_PASS ):
+           if ( re.search ( self.config_data [ "password_prompt" ], text ) ):
+               print ( "Password prompt detected." )
+               self._send_string ( self.config_data [ "password" ] )
+               self.state = MudFuzzState.AWAIT_PASS
+
+    def _send_string ( self, s ):
+
+        s += "\r\n"
+
+        if ( self.connection is None ):
+            return
+        b = s.encode ( 'utf-8' )
+        self.connection.send_queue.put_nowait ( b )
 
 class MudConnection:
 
@@ -45,11 +75,11 @@ class MudConnection:
         self.send_queue = queue.Queue ()
         self.rcv_queue = queue.Queue ()
 
-        self.rcv_thread = threading.Thread ( target=self._telnet_rcv,
-                                             daemon=True )
-        self.rcv_thread.start ()
+        self.telnet_thread = threading.Thread ( target=self._telnet_process,
+                                                daemon=True )
+        self.telnet_thread.start ()
 
-    def _telnet_rcv ( self ):
+    def _telnet_process ( self ):
         with Telnet ( self.host, self.port ) as tn:
             while True:
                 try:
@@ -57,7 +87,18 @@ class MudConnection:
                     self.rcv_queue.put ( rcv )
                 except EOFError:
                     break
-            time.sleep ( 0.1 )
+
+                try:
+                    send = self.send_queue.get( block=False )
+                except queue.Empty:
+                    pass
+                else:
+                    try:
+                        tn.write ( send )
+                    except OSError:
+                        break
+
+                time.sleep ( 0.1 )
 
 
 def parse_config_file ( f ):
@@ -79,15 +120,6 @@ def main ():
     while True:
         mudfuzz.tick ()
         time.sleep ( 0.1 )
-
-        #tn.read_until ( bytes (  config_data [ "user_prompt" ], "utf-8" ) )
-        #print ( "User prompt")
-        #tn.write ( config_data [ "user" ].encode ('utf-8') )
-        #tn.write ( "\r\n".encode ('utf-8') )
-        #tn.read_until ( bytes (  config_data [ "user_prompt" ], "utf-8" ) )
-        #tn.write ( config_data [ "password" ].encode ('utf-8') )
-        #tn.write ( "\r\n".encode ('utf-8') )
-        #tn.interact ()
 
 if __name__ == "__main__":
     main ();
