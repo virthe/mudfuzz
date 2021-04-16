bf7f42b08ae6d7d127ab70c22f3b121af3166edb
Virthe <virthe@accursed-lands.net>
2021-04-16 16:52:15 +0100

    rm special word commands

--- mudfuzz/fuzz_commands/send_escape.py
+++
@@ -1,7 +0,0 @@
-from mudfuzz.fuzz_commands.fuzz_command import FuzzCommand
-
-class SendEscape ( FuzzCommand ):
-    def execute ( self, mudfuzz ):
-        mudfuzz.send_eol ()
-        mudfuzz.send_string ( "**" )
-        mudfuzz.send_eol ()
--- mudfuzz/fuzz_commands/send_word.py
+++
@@ -1,9 +0,0 @@
-import random
-from mudfuzz.fuzz_commands.fuzz_command import FuzzCommand
-
-class SendWord ( FuzzCommand ):
-    def execute ( self, mudfuzz ):
-        mudfuzz.send_string ( 
-        random.choice ( mudfuzz.config_data.valid_words ))
-        mudfuzz.send_string ( " " )
-
