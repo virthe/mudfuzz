2cc301d950a72707eb593e2f7ef9cb09430451c4
Virthe <virthe@accursed-lands.net>
2021-04-17 15:00:56 +0100

    linting

--- mudfuzz/util.py
+++ mudfuzz/util.py
@@ -1,10 +1,10 @@
 import re
 
-def strip_ansi ( text ):
+
+def strip_ansi(text):
     ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
     return ansi_escape.sub('', text)
 
-def snake_to_camel_case ( text ):
+
+def snake_to_camel_case(text):
     return "".join([x.title() for x in text.split("_")])
-
-
